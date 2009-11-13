18e81cbee983d9e6e03b38bc812c37c5fdcd3b2c
Andrew Sardone <andrew.sardone@gmail.com>
2009-11-12 23:38:53 -0500

    Initial index list function
    Scaffolded by `couchapp generate list index` with some custom code

---
+++ lists/index.js
@@ -0,0 +1,32 @@
+function(head, req) {
+  // !json templates
+  // !code vendor/couchapp/path.js
+  // !code vendor/couchapp/date.js
+  // !code vendor/couchapp/template.js
+  
+  var indexPath = listPath('index', 'recent-bookmarks', { descending:true, limit: 5 });
+  
+  // The provides function serves the format the client requests.
+  // The first matching format is sent, so reordering functions changes 
+  // thier priority. In this case HTML is the preferred format, so it comes first.
+  provides("html", function() {
+    // redner the html head using a template
+    send(template(templates.layout.head, {
+      title: "foo",
+      index: indexPath,
+      assets: assetPath(),
+      show: showPath()
+    }));
+    
+    // loop over view rows, rendering one at a time
+    var row, key;
+    while (row = getRow()) {
+      // see sofa's lists/index.js
+    }
+    
+    // render the html tail template
+    return template(templates.layout.tail, {
+      assets: assetPath()
+    });
+  });
+}
